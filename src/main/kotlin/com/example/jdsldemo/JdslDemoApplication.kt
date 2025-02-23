4377bcb221202d10d8c33329ec867ca995918c8d
choi-sangyong <csy7792@naver.com>
2025-02-23 15:35:52 +0900

    feature: project init

---
+++ src/main/kotlin/com/example/jdsldemo/JdslDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jdsldemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JdslDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<JdslDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/jdsldemo/JdslDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jdsldemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JdslDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
